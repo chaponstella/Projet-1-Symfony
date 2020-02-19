a9c18eb1a944c1d2e0f92909b582dbe17c07050f
chaponstella <chapon.stella01@gmail.com>
2020-02-19 16:52:18 +0100

    commit

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,7 +18,7 @@ class ArticlesController extends AbstractController
     }
 
     /**
-     * @Route("/articles", name="articles")
+     * @Route("/articles", methods={"GET"}, name="articles")
      */
     public function index(): Response
     {
@@ -37,4 +38,13 @@ class ArticlesController extends AbstractController
             'articleId' => $id
         ]);
     }
+
+      /**
+     * @Route("/articles/", methods={"POST"}, name="article_create")
+     */ 
+    public function create(Request $request)
+    {
+        dd($request);
+    }
+
 }
---
+++ src/Entity/Article.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $short_content;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $tags;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function __construct() {
+        $this-> setCreatedAt(new DateTime());
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getShortContent(): ?string
+    {
+        return $this->short_content;
+    }
+
+    public function setShortContent(?string $short_content): self
+    {
+        $this->short_content = $short_content;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getTags(): ?string
+    {
+        return $this->tags;
+    }
+
+    public function setTags(?string $tags): self
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
