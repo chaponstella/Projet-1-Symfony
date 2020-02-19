b0331036d3ba383c72b6b6e4b0708ead376112ff
chaponstella <chapon.stella01@gmail.com>
2020-02-19 14:41:57 +0100

    :tada: Init repository

---
+++ config/ArticlesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticlesController extends AbstractController
+{
+    /**
+     * @Route("/articles/new", name="articles_new")
+     */
+    public function new(): Response
+    {
+        return $this->render('articles/new.html.twig');
+    }
+
+    /**
+     * @Route("/articles", name="articles")
+     */
+    public function index(): Response
+    {
+        return $this->render('articles/index.html.twig', [
+            'controller_name' => 'ArticlesController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/articles/{id}", name="articles_show", requirements={"id"="\d+"})
+     */ 
+    public function show(int $id): Response
+    {
+
+        return $this->render('articles/show.html.twig', [
+            'articleId' => $id
+        ]);
+    }
+}
---
+++ src/Controller/PagesController.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class PagesController {
+    public function about() : Response
+    {
+return new Response("Page ABOUT !");
+    }
+}
